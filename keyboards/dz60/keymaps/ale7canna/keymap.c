de6af63810dfbd78f97c69d755cb94a745414d2d
canale <alessandro.canicatti@tcpos.com>
2019-05-07 13:32:18 +0200

    G and H work as HYPER

--- keyboards/dz60/keymaps/ale7canna/keymap.c
+++ keyboards/dz60/keymaps/ale7canna/keymap.c
@@ -21,6 +21,8 @@
 #define MY_SPC LT(FUNL, KC_SPC)
 #define MY_A LT(CURS, KC_A)
 #define MY_Q LT(MEDI, KC_Q)
+#define MY_G MT(MOD_HYPR, KC_G)
+#define MY_H MT(MOD_HYPR, KC_H)
 #define GT_TEMP LT(TEMP, KC_GRV)
 #define GT_DANG LT(DANG, KC_NO)
 
@@ -29,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT(
 		GT_TEMP, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_NO  ,  KC_GRV,
 		KC_TAB , MY_Q   , KC_W   , MY_E   , KC_R   , KC_T   , KC_Y   , KC_U   , MY_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,     KC_BSLS     ,
-		KC_ESC , MY_A   , MY_S   , MY_D   , MY_F   , KC_G   , KC_H   , MY_J   , MY_K   , MY_L   , KC_SCLN, KC_QUOT,          KC_ENT          ,
+		KC_ESC , MY_A   , MY_S   , MY_D   , MY_F   , MY_G   , MY_H   , MY_J   , MY_K   , MY_L   , KC_SCLN, KC_QUOT,          KC_ENT          ,
 		KC_BSPC, KC_NO  , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,       KC_DEL    ,   KC_NO,
 		KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                   MY_SPC                   , KC_NO  , KC_NO  , KC_APP , KC_NO  , KC_CAPS,  KC_NO ),
 
