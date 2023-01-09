292ae972a9553109dbe20bfcd79b1757f6eb85fc
Alessandro Canicatti <alessandro.canicatti@cloudacademy.com>
2023-01-09 10:21:14 +0100

    feat: updated compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,6 @@
-sleep 5 && find /run/media/ale7canna/TADA68 -type f | xargs -f && cp .build/tada68_ale7canna.bin /run/media/ale7canna/TADA68/build.bin
+ docker build -t qmk .
+docker run -it -v $(pwd):/qmk_firmware qmk make dz60:ale7canna:bin
+1732  1/9/2023 10:18  sudo dfu-programmer atmega32u4 read
+ 1733  1/9/2023 10:18  sudo dfu-programmer atmega32u4 erase --force\n
+ 1734  1/9/2023 10:19  sudo dfu-programmer atmega32u4 flash .build/dz60_ale7canna.hex
+ 1735  1/9/2023 10:19  sudo dfu-programmer atmega32u4 reset
